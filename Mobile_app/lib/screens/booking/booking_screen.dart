87d906d18d2d7152be65d8aa7b22f9fdad0443fa
Tharo Chan <tharochanrith@gmail.com>
2024-06-19 23:46:51 +0700

    fix: dry run

--- Mobile_app/lib/screens/booking/booking_screen.dart
+++ Mobile_app/lib/screens/booking/booking_screen.dart
@@ -5,7 +5,7 @@ class BookingScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: Text('Calendar Page'),
     );
   }
