2a446a97e97f342e2fb2191aad690115554b5b64
Tharo Chan <tharochanrith@gmail.com>
2024-06-20 13:51:54 +0700

    feat: Appointment section

--- Mobile_app/lib/screens/home/home_screen.dart
+++ Mobile_app/lib/screens/home/home_screen.dart
@@ -1,3 +1,5 @@
+import 'package:doc_care/shared/widgets/cards/appointment_preview_card.dart';
+import 'package:doc_care/shared/widgets/titiles/section_title.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -90,105 +92,35 @@ class HomeView extends StatelessWidget {
           ),
         ),
       ),
+      body: const SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Column(
+            children: <Widget>[
+              _MyAppointment(),
+              SizedBox(height: 24.0),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
 
-// class _DoctorCategories extends StatelessWidget {
-//   const _DoctorCategories();
+class _MyAppointment extends StatelessWidget {
+  const _MyAppointment();
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return BlocBuilder<HomeBloc, HomeState>(
-//       builder: (context, state) {
-//         return Column(
-//           children: [
-//             SectionTitle(
-//               title: 'Categories',
-//               action: 'See all',
-//               onPressed: () {},
-//             ),
-//             Row(
-//               mainAxisAlignment: MainAxisAlignment.start,
-//               children: state.doctorCategories
-//                   // Take 5 could be added in the bloc calculation
-//                   .take(5)
-//                   .map(
-//                     (category) => Expanded(
-//                       child: CircleAvatarWithTextLabel(
-//                         icon: category.icon,
-//                         label: category.name,
-//                       ),
-//                     ),
-//                   )
-//                   .toList(),
-//             ),
-//           ],
-//         );
-//       },
-//     );
-//   }
-// }
-
-// class _MySchedule extends StatelessWidget {
-//   const _MySchedule();
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return BlocBuilder<HomeBloc, HomeState>(
-//       builder: (context, state) {
-//         return Column(
-//           children: [
-//             SectionTitle(
-//               title: 'My Schedule',
-//               action: 'See all',
-//               onPressed: () {},
-//             ),
-//             AppointmentPreviewCard(
-//               appointment: state.myAppointments.firstOrNull,
-//             ),
-//           ],
-//         );
-//       },
-//     );
-//   }
-// }
-
-// class _NearbyDoctors extends StatelessWidget {
-//   const _NearbyDoctors();
-
-//   @override
-//   Widget build(BuildContext context) {
-//     final colorScheme = Theme.of(context).colorScheme;
-
-//     return BlocBuilder<HomeBloc, HomeState>(
-//       builder: (context, state) {
-//         return Column(
-//           children: [
-//             SectionTitle(
-//               title: 'Nearby Doctors',
-//               action: 'See all',
-//               onPressed: () {},
-//             ),
-//             const SizedBox(height: 8.0),
-//             ListView.separated(
-//               physics: const NeverScrollableScrollPhysics(),
-//               shrinkWrap: true,
-//               separatorBuilder: (context, index) {
-//                 return Divider(
-//                   height: 24.0,
-//                   color: colorScheme.surfaceVariant,
-//                 );
-//               },
-//               itemCount: state.nearbyDoctors.length,
-//               itemBuilder: (context, index) {
-//                 final doctor = state.nearbyDoctors[index];
-//                 return DoctorListTile(doctor: doctor);
-//               },
-//             ),
-//           ],
-//         );
-//       },
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SectionTitle(
+          title: 'Upcoming Appointments',
+          action: '',
+          onPressed: () {},
+        ),
+        const AppointmentPreviewCard(),
+      ],
+    );
+  }
+}
