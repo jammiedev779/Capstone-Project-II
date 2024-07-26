e21ab2de26b5c3b03e72d96c763e61d6c4626b4d
Shine <120034529+Kimle7@users.noreply.github.com>
2024-07-27 00:41:20 +0700

    solve conflict on main.dart in mobile

--- Mobile_app/lib/main.dart
+++ Mobile_app/lib/main.dart
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
 import 'package:doc_care/screens/login_signup/login_screen.dart';
 import 'package:doc_care/screens/login_signup/register_screen.dart';
-=======
-import 'package:doc_care/screens/login/login_screen.dart';
->>>>>>> 106331db243e753ec3690727ba675c8cb20cf74a
 import 'package:doc_care/shared/theme/app_theme.dart';
 import 'package:doc_care/shared/utils/bottom_nav_bars/main_nav_bar.dart';
 import 'package:flutter/material.dart';
@@ -19,12 +15,8 @@ class DocCare extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-<<<<<<< HEAD
       // home: const MainNavBar(),
       home: RegisterPage(),
-=======
-      home: LoginPage(),
->>>>>>> 106331db243e753ec3690727ba675c8cb20cf74a
       theme: const AppTheme().themeData,
     );
   }
