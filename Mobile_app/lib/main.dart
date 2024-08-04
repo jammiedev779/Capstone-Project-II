20f58a95b9bb3f5f77bc333fda5285c8328fed57
TSPanha <129873245+CPanha@users.noreply.github.com>
2024-08-04 18:37:44 +0700

    just change to working on it

--- Mobile_app/lib/main.dart
+++ Mobile_app/lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:doc_care/screens/home/home_screen.dart';
-import 'package:doc_care/screens/home/medical_center.dart';
 import 'package:doc_care/screens/home/notification_page.dart';
 import 'package:doc_care/screens/appointment/appointment_screen.dart';
 import 'package:doc_care/screens/hospital/hospital_list.dart';
@@ -16,15 +15,15 @@ void main() {
 }
 
 class DocCare extends StatelessWidget {
-  const DocCare({super.key});
+  const DocCare({super.key}); 
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       // home: MainNavBar(),
-      home: HospitalListScreen(),
-      // home: HomeScreen(),
+      // home:LoginPage(),
+      home: HomeScreen(),
       // home: MedicalHistory(),
       theme: const AppTheme().themeData,
     );
